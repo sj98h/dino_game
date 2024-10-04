813d291b4b9a884268bb5462b2ee8affa31e3170
sj98h <sj98h@naver.com>
2024-10-04 10:54:37 +0900

    스프라이트

--- public/index.js
+++ public/index.js
@@ -34,17 +34,15 @@ const CACTI_CONFIG = [
   { width: 50 / 1.5, height: 50 / 1.5, image: "images/cactus_1.png" },
   { width: 70 / 1, height: 70 / 1, image: "images/cactus_3.png" },
   { width: 99 / 1.5, height: 99 / 1.5, image: "images/cactus_2.png" },
-  { width: 100 / 1.5, height: 100 / 1.5, image: "images/테스트용1-removebg-preview.png" },
 ];
 
 // 아이템
 const ITEM_CONFIG = [
-  { width: 50 / 1.5, height: 50 / 1.5, id: 1, image: "images/items/pokeball_red.png" },
-  { width: 50 / 1.5, height: 50 / 1.5, id: 2, image: "images/items/pokeball_yellow.png" },
-  { width: 50 / 1.5, height: 50 / 1.5, id: 3, image: "images/items/pokeball_purple.png" },
-  { width: 50 / 1.2, height: 50 / 1.2, id: 4, image: "images/items/pokeball_cyan.png" },
-  { width: 50 / 1.1, height: 50 / 1.1, id: 5, image: "images/items/pokeball_orange.png" },
-  { width: 50 / 0.9, height: 50 / 0.9, id: 6, image: "images/items/pokeball_pink.png" },
+  { width: 50 / 1.5, height: 50 / 1.5, id: 1, image: "images/items/item1.png" },
+  { width: 50 / 1.5, height: 50 / 1.5, id: 2, image: "images/items/item2.png" },
+  { width: 50 / 1.5, height: 50 / 1.5, id: 3, image: "images/items/item3.png" },
+  { width: 50 / 1.2, height: 50 / 1.2, id: 4, image: "images/items/item4.png" },
+  { width: 50 / 1.1, height: 50 / 1.1, id: 5, image: "images/items/item5.png" },
 ];
 
 // 게임 요소들
